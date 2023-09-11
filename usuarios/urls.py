701e1c70d723b63c98f81b5ed5cec83a048542a0
LucasdoPradoTozzi <120129056+LucasdoPradoTozzi@users.noreply.github.com>
2023-09-11 17:38:35 -0300

    criando o caminho para cadastro e login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from usuarios.views import login, cadastro
 
 urlpatterns = [
-    path('login', login, name="login"),
-    path('cadastro', cadastro, name="cadastro"),
-   
+    path('login', login, name='login'),
+    path('cadastro', cadastro, name='cadastro'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def login(request):
-    return render("usuarios/login.html")
+    return render(request, "usuarios/login.html")
 
 def cadastro(request):
-    return render("usuarios/cadastro.html")
+    return render(request, "usuarios/cadastro.html")
