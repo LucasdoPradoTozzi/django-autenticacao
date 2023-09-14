f1646d3bb45d4d45a14f7c29472dc05b7230bd60
LucasdoPradoTozzi <120129056+LucasdoPradoTozzi@users.noreply.github.com>
2023-09-13 23:10:13 -0300

    criando sistema de login e validação de dados

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def login(request):
     form = LoginForms()
+
+    if form.is_valid():
+        nome=form['nome_login'].value()
+        senha=form['senha'].value()
+
+    usuario = auth.authenticate(
+        request,
+        username=nome,
+        password=senha
+    )
+    if usuario is not None:
+        auth.login(request, usuario)
+        return redirect('index')
+    else:
+        return redirect('login')
+        
     return render(request, "usuarios/login.html", {"form": form})
 
 def cadastro(request):
@@ -11,7 +29,28 @@ def cadastro(request):
     if request.method == 'POST':
         form = CadastroForms(request.POST)
 
-        if form["senha_1"].value() != form["senha_2"].value():
-            return redirect('cadastro')
+        if form.is_valid():
+            if form["senha_1"].value() != form["senha_2"].value():
+                return redirect('cadastro')
+            
+            nome=form["nome_cadastro"].value()
+            e_mail=form["email"].value()
+            senha=form["senha_1"].value()
+
+            if User.objects.filter(username=nome).exists():
+                return redirect('cadastro')
+            
+            if User.objects.filter(email=e_mail).exists():
+                return redirect('cadastro')
+            
+            usuario = User.objects.create_user(
+                username=nome,
+                email=e_mail,
+                password=senha
+            )
+            usuario.save()
+            return redirect('login')
+        
+
         
     return render(request, "usuarios/cadastro.html", {"form": form})
