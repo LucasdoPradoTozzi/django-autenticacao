389c9fa1a5f422a795ac194a3e87dd7a588dad2d
LucasdoPradoTozzi <120129056+LucasdoPradoTozzi@users.noreply.github.com>
2023-09-19 23:11:41 -0300

    adicionando formulario semi funcional de nova imagem

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
 from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
+from django.contrib.auth.models import User
+from django.contrib import auth
 from django.contrib import messages
 
 def index(request):
@@ -28,7 +31,19 @@ def buscar(request):
     return render(request, "galeria/buscar.html", {"cards": fotografias})
 
 def nova_imagem(request):
-    return render(request, 'galeria/nova_imagem.html')
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado!")
+        return redirect('login')
+     
+    form = FotografiaForms
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Nova imagem cadastrada!')
+            return redirect('index')
+
+    return render(request, 'galeria/nova_imagem.html', {'form': form})
 
 def editar_imagem(request):
     pass
